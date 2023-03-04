66bf738f72a8cb46e06473bbae1f326680da8590
Hubert Muller <alotofgreatideas@gmail.com>
2023-03-04 12:07:11 +0100

    attrybuty

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ console.log("Ostatni element to: ", ostatniElementTd);
 
 const pierwszyTd = document.querySelector("td");
 console.log("Pierwszy element to: ", pierwszyTd);
+
+//document.getElementById("element1");
+//document.getElementsByTagName("table");
+//document.getElementsByName("nazwisko");
+//document.getElementsByClassName("tekscik");
+//....getElements.... zawsze zwroci nam tablice/kolekcje, 
+// a getElementById zawsze pojedynczy element
+
+const el = document.getElementsByName("nazwisko")[0];
+
+console.log('element o name=nazwisko ma nastepujace atrybuty', el.attributes);
+
+const maType = el.hasAttribute("type");
+console.log('element o name=nazwisko ' + (maType?' ma atrybut type':'nie ma atrybutu type') );
+
+//.getAttribute
+//.setAttribute
+//.removeAttribute
