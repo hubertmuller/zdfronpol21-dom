8ca77bae059407c9c6735f4c46fd7ef3de20f598
Hubert Muller <alotofgreatideas@gmail.com>
2023-03-04 10:57:27 +0100

    queryselector

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const ostatniElementTd = elementyTd[elementyTd.length-1];
 console.log("Bieżący dokument zawiera " + elementyTd.length + " elementów td");
 console.log("Pierwszy element to: ", pierwszyElementTd);
 console.log("Ostatni element to: ", ostatniElementTd);
+
+const pierwszyTd = document.querySelector("td");
+console.log("Pierwszy element to: ", pierwszyTd);
