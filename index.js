869d03d76e0ca280356ad17c552be4458a337be7
Hubert Muller <alotofgreatideas@gmail.com>
2023-03-04 10:52:59 +0100

    querySelectorAll

---
+++ index.js
@@ -0,0 +1,6 @@
+const elementyTd = document.querySelectorAll("td");
+const pierwszyElementTd = elementyTd[0];
+const ostatniElementTd = elementyTd[elementyTd.length-1];
+console.log("Bieżący dokument zawiera " + elementyTd.length + " elementów td");
+console.log("Pierwszy element to: ", pierwszyElementTd);
+console.log("Ostatni element to: ", ostatniElementTd);
