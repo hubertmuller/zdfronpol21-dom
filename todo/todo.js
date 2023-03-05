0cf06d9ce980ef5ee2fc9d67e48f3626f4d6429e
Hubert Muller <alotofgreatideas@gmail.com>
2023-03-05 12:58:33 +0100

    dodanaie elementu

--- todo/todo.js
+++ todo/todo.js
@@ -6,10 +6,21 @@ function znajdzElement(selektor) {
     return document.querySelector(selektor);
 }
 
+function zrobElement(nazwa,tekst) {
+    const element = document.createElement(nazwa);
+    element.innerText=tekst;
+    return element
+}
+
 const lista = znajdzElement("ul");
 const dodaj = znajdzElement("button");
 const input = znajdzElement("input[name='rzecz']");
 
 dodaj.addEventListener("click", () => {
     const rzecz = input.value;
+    input.value = "";
+    const elLi = zrobElement("li", rzecz);
+    lista.appendChild(elLi);
 })
+
+
