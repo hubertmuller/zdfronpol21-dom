6871202663b40fc1773034ee11f2d85c4b0cdd87
Hubert Muller <alotofgreatideas@gmail.com>
2023-03-05 15:52:39 +0100

    kolejnosc

--- todo/todo.js
+++ todo/todo.js
@@ -45,10 +45,30 @@ dodaj.addEventListener("click", () => {
     const rzecz = input.value;
     if (input.value !== ""){
         input.value = "";
+        //tworzymy element <li>
         const elLi = zrobElement("li", rzecz);
+        //Twprzenie przyciskow dodawany do <li>
         const elUsun = zrobElement("button","usun");
+        const elGora = zrobElement("button","w górę");
+        const elGotowe = zrobElement("button","zrobione");
+        //dodawanie elementu li do ul
         lista.appendChild(elLi);
+        //dodawanie przyciskow (appendchild)
         elLi.appendChild(elUsun);
+        elLi.appendChild(elGora);
+        elLi.appendChild(elGotowe);
+        elUsun.onclick = (evt) => {
+            evt.target.parentElement.remove();
+        }
+        elGotowe.onclick = (evt) => {
+            evt.target.parentElement.style.textDecoration = "line-through";
+        }
+        elGora.onclick = (evt) => {
+            const liKlikniete = evt.target.parentElement;
+            const liPoprzedzajacy = liKlikniete.previousElementSibling;
+            const ul = liKlikniete.parentElement;
+            ul.insertBefore(liKlikniete, liPoprzedzajacy);
+        }
         pokazKomunikat('dodano czynnosc', 0);
     }
     else {
